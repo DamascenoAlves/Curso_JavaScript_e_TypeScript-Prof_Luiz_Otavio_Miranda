c93c0fc0d09e5a719b4888ab7dee910949bbd877
Matheus Damasceno Alves <matheusd.alves@hotmail.com>
2024-02-16 01:22:11 -0300

    Executadas aulas 9, 10 e 11

---
+++ CursoJS/aula010/index.js
@@ -0,0 +1,31 @@
+/*
+*   Aritméticos
+*   + Adição / Concatenação
+*   - / * Soma, divisão e multiplicação
+*   ** potenciação
+*   % Resto da divisão
+*
+*/
+/*
+Precedência:
+     ()
+     **
+    * / %
+     + -
+*/
+/*
+    Incremento: ++
+    Decremento: --
+*/
+/*
+    NaN - Not a number
+    parseInt (inteiro)
+    parseFloat(decimais)
+*/
+
+// NaN - Not a number parseInt (inteiro), parseFloat(decimais)
+const num1 = 10;
+const num2 = Number('Luiz');
+
+console.log(num1 + num2);
+console.log(typeof num2);
---
+++ CursoJS/aula011/js/script.js
@@ -0,0 +1,3 @@
+let num1 = Number(prompt('Digite um número'))
+let num2 = Number(prompt('Digite outro número'))
+alert(`O resultado é ${num1+num2}`)
